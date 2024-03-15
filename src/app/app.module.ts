4255729582f60881ad6fb178d10c8dc267ed19c6
MBY13 <matheusbayersbs@gmail.com>
2024-03-15 19:52:58 -0300

    Criando_Tela

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 import { RecuperarSenhaComponent } from './recuperar-senha/recuperar-senha.component';
 import { CriarCadastroComponent } from './criar-cadastro/criar-cadastro.component';
 
@@ -46,7 +48,9 @@ const routes: Routes = [
     MatCardModule,
     MatFormFieldModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
   ],
   declarations: [
     AppComponent,
--- src/app/criar-cadastro/criar-cadastro.component.ts
+++ src/app/criar-cadastro/criar-cadastro.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-criar-cadastro',
   templateUrl: './criar-cadastro.component.html',
   styleUrls: ['./criar-cadastro.component.css']
 })
-export class CriarCadastroComponent implements OnInit {
+export class CriarCadastroComponent {
+  username: string;
+  dob: Date;
+  email: string;
+  login: string;
+  senha: string;
+  selectedIntegration: string;
+  integrationList: string[] = ['Integração 1', 'Integração 2', 'Integração 3'];
+  registrationError: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  submitRegistrationForm() {
+    // Aqui você pode adicionar a lógica para enviar os dados do formulário
+    // Para simplificar, vamos apenas imprimir os dados no console
+    console.log('Dados do Formulário:');
+    console.log('Nome de Usuário:', this.username);
+    console.log('Data de Nascimento:', this.dob);
+    console.log('Email:', this.email);
+    console.log('Login:', this.login);
+    console.log('Senha:', this.senha);
+    console.log('Integração Selecionada:', this.selectedIntegration);
+
+    // Você pode adicionar a lógica de envio para o seu serviço ou API aqui
   }
-
 }
